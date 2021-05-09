b9cf828e83ed930722ad335dffba12d4f1bb19d1
MWBairam <71559907+MWBairam@users.noreply.github.com>
2021-05-10 00:57:01 +0300

    Logging Exceptions to a file with NLog

--- admin/Program.cs
+++ admin/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,18 @@ namespace admin
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                }).ConfigureLogging((hostingContext, loggingBuilder) =>
+                {
+                    //githib official page for CreateDefaultBuilder: https://github.com/dotnet/aspnetcore/blob/main/src/DefaultBuilder/src/WebHost.cs
+                    //we want to add NLog logging provider to log to files.
+                    //in order to add it, we should use the .ConfigureLogging,
+                    //Console and Debug and EventSource logging providers were configured by default, so overriding .ConfigureLogging here will delete them,
+                    //so write them again.
+                    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    loggingBuilder.AddConsole();
+                    loggingBuilder.AddDebug();
+                    loggingBuilder.AddEventSourceLogger();
+                    loggingBuilder.AddNLog();
                 });
     }
 }
